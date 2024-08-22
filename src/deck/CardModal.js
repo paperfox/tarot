0fb980c2ef51f935c70c46723178beeabdeee4a8
paperfox <ndg.paperfox@gmail.com>
2024-08-21 23:07:48 -0400

    Tarot - update react-bootstrap, clean up

--- src/deck/CardModal.js
+++ src/deck/CardModal.js
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 
-function CardModal({ reverseCard, dataImg, dataContent }) {
+function CardModal({ dataImg, dataContent }) {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
