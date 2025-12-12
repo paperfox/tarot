6c6376d562c0042846a081bed33ee4f762281906
Nathalie Garfinkle <ndg.paperfox@gmail.com>
2025-12-11 21:32:44 -0500

    update gemini

--- src/daily/CardDaily.jsx
+++ src/daily/CardDaily.jsx
@@ -18,7 +18,7 @@ function CardDaily({ tarotContent }) {
   const [binary, setBinary] = useState('');
 
   const genAI = new GoogleGenerativeAI(MY_KEY);
-  const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash' });
+  const model = genAI.getGenerativeModel({ model: 'gemini-2.5-flash' });
 
   var loading = disabled === true ? 'skeleton-loading' : '';
   let date = new Date().toLocaleDateString();
